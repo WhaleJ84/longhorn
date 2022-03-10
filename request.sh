49f8beb6d4cc7c24e663ca31fd0dabd8dbcdaf8b
James Whale <jamwhale@cisco.com>
2022-03-10 12:21:17 +0000

    removed trailing comma from body

--- request.sh
+++ request.sh
@@ -4,7 +4,6 @@ curl -X POST \
   'http://127.0.0.1:5000/link-down/' \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer dGhpc19pc19pbnNlY3VyZQ==' \
   -d '{
-  	'causing_ci': 'hostname.example.com',
+  	"causing_ci": "hostname.example.com"
   }'
