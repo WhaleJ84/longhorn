ba027033bfc19a2d602087d46742dc190cab1535
James Whale <jamwhale@cisco.com>
2022-03-12 14:28:40 +0000

    removed erroneous quotes from header

--- request.sh
+++ request.sh
@@ -4,7 +4,7 @@ curl -X POST \
   'http://127.0.0.1:5000/link-down/' \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
-  -H 'Authorization': 'Bearer dGhpc19pc19pbnNlY3VyZQ==' \
+  -H 'Authorization: Bearer dGhpc19pc19pbnNlY3VyZQ==' \
   -d '{
   	"causing_ci": "hostname.example.com",
   	"event_text": "LINK DOWN | TRANSIT-LON<>CAR | A3",
